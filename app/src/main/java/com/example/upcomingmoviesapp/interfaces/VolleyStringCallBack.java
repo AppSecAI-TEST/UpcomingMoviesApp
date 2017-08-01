980dac16d1dd4e66faf1b79f010fc55fdd344900
omandev9 <omandev9@gmail.com>
2017-08-01 23:19:38 +0530

    webservice interface removed

--- app/src/main/java/com/example/upcomingmoviesapp/interfaces/VolleyStringCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.example.upcomingmoviesapp.interfaces;
-
-public interface VolleyStringCallBack {
-
-    public void getResponseString(String dataString, String tag);
-
-    public void getErrorString(String error);
-
-}
